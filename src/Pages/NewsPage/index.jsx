a906c1c8eeaeaeeb6d89be54cca85c0554aa12ff
lucas chagas <lucazchagaz@outlook.com>
2022-10-03 15:49:07 -0300

    implementacao do componente de pesquisa

--- src/Pages/NewsPage/index.jsx
+++ src/Pages/NewsPage/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Search from "../../Components/Search"
 
 const NewsPage = ({ limit }) => {
   return (
-    <div>
-      
-    </div>
+    <div className="Currencies-Conteiner">
+    <h1>Notícias</h1>
+    {!limit && 
+      <Search />
+    }
+  </div> 
   )
 }
 
