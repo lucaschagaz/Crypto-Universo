f2dbc49f498062f62f0012748df7769ebe4d25bb
lucas chagas <lucazchagaz@outlook.com>
2022-10-03 15:46:19 -0300

    criação da api de noticias

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,13 +1,25 @@
 import { createApi, fetchBaseQuery  } from  "@reduxjs/toolkit/query/react"
 
-const baseUrl = "https://coinranking1.p.rapidapi.com"
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/news/search';
+
+const CurrenciesNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '4a2e6efff4msh4078238201c4fe3p11d7f1jsnfd547a3e95b1',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+}
 
 export const NewsApi = createApi({
     reducerPath:"NewsApi",
     baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints:(builder)=>{
-        getNews:bulder.query({
-            query:
+    endpoints:(builder)=>({
+        getNews:builder.query({
+            query:(count)=>{
+                return{
+                    url:"/",
+                    headers: CurrenciesNewsHeaders,
+                    params: {q: 'bitcoin', safeSearch: 'Off', textFormat: 'Raw', freshness: 'Day', limit:count}         
+                }
+            }
         })
-    }
+    })
 })
