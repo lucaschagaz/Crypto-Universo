875017c295c1913f6e7a0277f3a00cf85bbc1b0a
Lucas chagas <lucazchagaz@outlook.com>
2022-11-28 23:58:10 -0300

    update on the components

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import CryptoContext from "./Contexts/LoggedContext"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-
 root.render(
   <React.StrictMode>
     <CryptoContext>
